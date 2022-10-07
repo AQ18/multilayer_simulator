59de5e1c14caa3ebc107380e5ce465f096a10ba9
Alex Qiu <alex.qiu@bristol.ac.uk>
2022-10-07 16:18:59 +0100

    Engines for Lumerical STACK solvers

--- multilayer_simulator/lumerical_classes.py
+++ multilayer_simulator/lumerical_classes.py
@@ -1,29 +1,25 @@
-from typing import ClassVar, Dict
+from typing import ClassVar, Dict, Iterable, Mapping, Optional
 import numpy as np
 from numpy.typing import NDArray
-from attrs import frozen
+from attrs import frozen, field, Factory
 import lumapi
 
 from engine import Engine
+from helpers.helpers import filter_mapping
 from structure import Structure
 
 
 @frozen
-class LumericalSTACK(Engine):
-    _properties_mapping_for_stackfield: ClassVar[Dict[str, str]] = {
-        'field_resolution': 'res',
-        'field_min': 'min',
-        'field_max': 'max'
-    }
-    
+class STACKRT(Engine):
     session: lumapi.FDTD
-    field_resolution: int = 1000
-    field_min: float = 0
-    field_max: float = field()
-    
-    @field_max.default
-    
-    def stackrt(self, index: NDArray[np.float_], thickness: NDArray[np.float_], frequencies: NDArray[np.float_], angles: NDArray[np.float_]) -> Dict[str, NDArray[np.float_]]:
+
+    def __call__(
+        self,
+        index: NDArray[np.float_],
+        thickness: NDArray[np.float_],
+        frequencies: NDArray[np.float_],
+        angles: NDArray[np.float_],
+    ) -> Dict[str, NDArray[np.float_]]:
         """
         Thin wrapper around lumapi.FDTD.stackrt.
 
@@ -38,16 +34,165 @@ class LumericalSTACK(Engine):
         :return: _description_
         :rtype: Dict[str, NDArray[np.float_]]
         """
-        
-        result = self.session.stackrt(n=index, d=thickness, f=frequencies, t=angles)
+        n = index
+        d = thickness
+        f = frequencies
+        theta = angles
+
+        result = self.session.stackrt(n, d, f, theta)
         return result
-    
+
+    def simulate(
+        self,
+        structure: Structure,
+        frequencies: NDArray[np.float64],
+        angles: NDArray[np.float64],
+        **kwargs
+    ):
+        """
+        Simulate the propagation of light through the structure in one dimension using Lumerical's STACK solver.
+        Returns the reflection and transmission coefficients.
+
+        :param structure: _description_
+        :type structure: Structure
+        :param frequencies: _description_
+        :type frequencies: NDArray[np.float64]
+        :param angles: _description_
+        :type angles: NDArray[np.float64]
+        :return: _description_
+        :rtype: _type_
+        """
+        index = structure.index(frequencies)
+        thickness = structure.thickness
+        data = self(index, thickness, frequencies, angles)
+        return data
+
+
+@frozen
+class STACKFIELD(Engine):
+    _allowed_kwargs: ClassVar[Iterable[str]] = ["resolution", "min", "max"]
+    session: lumapi.FDTD
+
+    def __call__(
+        self,
+        index: NDArray[np.float_],
+        thickness: NDArray[np.float_],
+        frequencies: NDArray[np.float_],
+        angles: NDArray[np.float_],
+        resolution: Optional[int] = None,
+        min: Optional[float] = None,
+        max: Optional[float] = None,
+    ) -> Dict[str, NDArray[np.float_]]:
+        """
+        Thin wrapper around lumapi.fdtd.stackfield which handles the optional rel, min, and max arguments.
+
+        :param index: _description_
+        :type index: NDArray[np.float_]
+        :param thickness: _description_
+        :type thickness: NDArray[np.float_]
+        :param frequencies: _description_
+        :type frequencies: NDArray[np.float_]
+        :param angles: _description_
+        :type angles: NDArray[np.float_]
+        :param resolution: _description_, defaults to None
+        :type resolution: Optional[int], optional
+        :param min: _description_, defaults to None
+        :type min: Optional[float], optional
+        :param max: _description_, defaults to None
+        :type max: Optional[float], optional
+        :return: _description_
+        :rtype: Dict[str, NDArray[np.float_]]
+        """
+        n = index
+        d = thickness
+        f = frequencies
+        theta = angles
+        args = []
+        if resolution is not None:  # Why this bizarre nested structure?
+            args.append(resolution)  # Because stackfield only takes positional args
+            if (
+                min is not None
+            ):  # TODO Reimplement this such that max can be set independently of res and min
+                args.append(min)
+                if max is not None:
+                    args.append(max)
+
+        result = self.session.stackfield(n, d, f, theta, *args)
+        return result
+
     @classmethod
-    def _parse_kwargs_for_stackfield(cls, **kwargs):
-        allowed_
-    
-    def stackfield(self, index: NDArray[np.float_], thickness: NDArray[np.float_], frequencies: NDArray[np.float_], angles: NDArray[np.float_], **kwargs
-                   ) -> Dict[str, NDArray[np.float_]]:
-    
-    def simulate(self, structure: Structure, frequencies: NDArray[np.float_], angles: NDArray[np.float_], **kwargs):
-        
+    def filter_kwargs(cls, **kwargs):
+        filtered_kwargs = filter_mapping(kwargs, ["resolution", "min", "max"])
+        return filtered_kwargs
+
+    def simulate(
+        self,
+        structure: Structure,
+        frequencies: NDArray[np.float64],
+        angles: NDArray[np.float64],
+        **kwargs
+    ):
+        """
+        Simulate the propagation of light through the structure in one dimension using Lumerical's STACK solver.
+        Returns the field profile between min and max with the given resolution.
+
+        :param structure: _description_
+        :type structure: Structure
+        :param frequencies: _description_
+        :type frequencies: NDArray[np.float64]
+        :param angles: _description_
+        :type angles: NDArray[np.float64]
+        :return: _description_
+        :rtype: _type_
+        """
+        index = structure.index(frequencies)
+        thickness = structure.thickness
+        filtered_kwargs = self.filter_kwargs(**kwargs)
+
+        data = self(index, thickness, frequencies, angles, **filtered_kwargs)
+        return data
+
+
+@frozen
+class LumericalSTACK(Engine):
+    _kwargs_substitutions: ClassVar[Mapping[str, str]] = {
+        "field_resolution": "resolution",
+        "field_min": "min",
+        "field_max": "max",
+    }
+
+    session: lumapi.FDTD
+    stackrt: STACKRT = Factory(lambda self: STACKRT(self.session), takes_self=True)
+    stackfield: STACKFIELD = Factory(
+        lambda self: STACKFIELD(self.session), takes_self=True
+    )
+
+    def simulate(
+        self,
+        structure: Structure,
+        frequencies: NDArray[np.float_],
+        angles: NDArray[np.float_],
+        **kwargs
+    ):
+        """
+        Simulate the propagation of light through the structure in one dimension using Lumerical's STACK solver.
+        Returns the output of STACKRT.simulate() and STACKFIELD.simulate() in a tuple.
+
+        :param structure: _description_
+        :type structure: Structure
+        :param frequencies: _description_
+        :type frequencies: NDArray[np.float_]
+        :param angles: _description_
+        :type angles: NDArray[np.float_]
+        :return: _description_
+        :rtype: _type_
+        """
+        index = structure.index(frequencies)
+        thickness = structure.thickness
+        filtered_kwargs_for_stackfield = self.stackfield.filter_kwargs(**kwargs)
+        rt_data = self.stackrt(index, thickness, frequencies, angles)
+        field_data = self.stackfield(
+            index, thickness, frequencies, angles, **filtered_kwargs_for_stackfield
+        )
+
+        return rt_data, field_data
