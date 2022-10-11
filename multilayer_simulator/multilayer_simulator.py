36dc565d755095d5efb36e71112bc547a7b2a4d6
Alex Qiu <alex.qiu@bristol.ac.uk>
2022-10-11 11:43:23 +0100

    Add pass statements

--- multilayer_simulator/multilayer_simulator.py
+++ multilayer_simulator/multilayer_simulator.py
@@ -8,22 +8,22 @@ class Visualisation:
     """
     Visualise a Simulation output.
     """
-    
+    pass
 class SaveData:
-    
+    pass
 class LoadData:
-    
+    pass
 class ConvertData:
     """
     Convert data from an Engine to some standard format.
     """
-    
+    pass
     
 class Processing:
     """
     Do maths on data.
     """
-    
+    pass
 def main():
     """
     The function which controls everything?
