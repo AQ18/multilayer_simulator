fd2ce7c899fdfb049f3c6de86530965e3a7b5f08
Alex Qiu <alex.qiu@bristol.ac.uk>
2022-10-12 11:17:24 +0100

    Implement Multilayer; refactor Layer

--- multilayer_simulator/structure.py
+++ multilayer_simulator/structure.py
@@ -4,7 +4,7 @@ from abc import ABC, abstractmethod
 from typing import Callable, Literal
 import numpy as np
 from numpy.typing import NDArray
-from attrs import mutable, frozen, field
+from attrs import mutable, frozen, field, setters
 
 from multilayer_simulator.material import Material
 
@@ -27,21 +27,40 @@ class Layer(Structure):
     """
     Represent a single layer.
     """
-    index: Callable[[Structure, NDArray[np.float_], Literal[1, 2, 3], NDArray[np.float_]]] # TODO: Type this as callback protocol instead
-    thickness: float
+    _index: Callable[[NDArray[np.float_], Literal[1, 2, 3], NDArray[np.float_]], NDArray[np.float_]] # TODO: Type this as callback protocol instead
+    thickness: NDArray[np.float_] = field(converter=np.atleast_1d, on_setattr=setters.convert)
     
-    def __attrs_post_init__(self):
-        self.index = self.index.__get__(self) # bind index function as instance method
+    @thickness.default
+    def _thickness_default(self=None): # set as function so can be used in factory class methods
+        return 0
+    
+    # def __attrs_post_init__(self):
+    #     self._index = self._index.__get__(self) # bind index function as instance method - or use property approach?
     
     @classmethod
-    def from_material(cls, material: Material, thickness: float):
+    def from_material(cls, material: Material, thickness: float = _thickness_default()):
         index = material.index
         return cls(index, thickness)
-        
 
+    def index(self, frequencies: NDArray[np.float_], component: Literal[1, 2, 3] = 1) -> NDArray[np.float_]:
+        return self._index(frequencies, component)
+        
+    # @property
+    # def thickness(self) -> NDArray[np.float_]:
+    #     return np.atleast_1d(self._thickness)
 
 class Multilayer(Structure):
     """
     Represent a multilayer.
     """
-    pass
+    layers: list[Layer]
+
+    def index(self, frequencies: NDArray[np.float_], component: Literal[1, 2, 3] = 1) -> NDArray[np.float_]:
+        index_array = np.array([layer.index(frequencies, component) for layer in self.layers])
+        return index_array
+    
+    @property
+    def thickness(self) -> NDArray[np.float_]:
+        thickness_array = np.array([layer.thickness for layer in self.layers]).squeeze() # squeeze from nx1 to n array
+        return thickness_array
+        
