8e0d95badb5dbe19738bf42bfd7dab18034cbb65
Alex Qiu <alex.qiu@bristol.ac.uk>
2022-10-17 12:37:15 +0100

    Add OutputFormats type

--- multilayer_simulator/helpers/formatters.py
+++ multilayer_simulator/helpers/formatters.py
@@ -4,4 +4,5 @@ class DataFormatter:
     """
     Format the output from a given engine in some consistent way.
     """
+    OutputFormats = Literal[None, 'xarray_dataset', 'xarray_dataarray'] # allowed formats when passed as a parameter
     pass
