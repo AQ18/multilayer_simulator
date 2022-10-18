012295436339fa98f0edd1883bcac24a5d6c604d
Alex Qiu <alex.qiu@bristol.ac.uk>
2022-10-18 16:08:04 +0100

    Fill in init with main classes

--- multilayer_simulator/__init__.py
+++ multilayer_simulator/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """Alex Qiu"""
 __email__ = 'alex.qiu@bristol.ac.uk'
 __version__ = '0.1.0'
+
+from multilayer_simulator.structure import Layer, Multilayer
+from multilayer_simulator.simulation import Simulation
