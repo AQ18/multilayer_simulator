3ec8fb85bf3a4e335c0d3997e415e0d29a6f4019
Alex Qiu <alex.qiu@bristol.ac.uk>
2022-11-17 01:25:50 +0000

    Add Multilayer.from_unit_cell()

--- multilayer_simulator/structure.py
+++ multilayer_simulator/structure.py
@@ -91,3 +91,50 @@ class Multilayer(Structure):
             [layer.thickness for layer in self.layers]
         ).squeeze()  # squeeze from nx1 to n array
         return thickness_array
+
+    @classmethod
+    def from_unit_cell(
+        cls,
+        unit_cell: Iterable[Layer],
+        incident_layer: Layer = Layer.from_material(),
+        exit_layer: Layer = Layer.from_material(),
+        num_periods: int = 1,
+        copy_layers: bool = True,
+    ) -> 'Multilayer':
+        """
+        Return a periodic Multilayer containing some number of repetitions of a unit cell.
+        The Multilayer is not guaranteed to be periodic after instantiation - internal
+        Layer objects can be deleted or modified.
+        If copy_layers is True, then every Layer is deepcopied.
+        Otherwise, the Layers will be the same objects across unit cells,
+        and also across instances if they are used in multiple constructors.
+        This can save memory and simplify modifications to the unit cell, but may lead to
+        unexpected behaviour.
+
+        :param unit_cell: The layers that will be repeated to make up the multilayer
+        :type unit_cell: Iterable[Layer]
+        :param incident_layer: Layer defining the incident medium, defaults to Layer.from_material()
+        :type incident_layer: Layer, optional
+        :param exit_layer: Layer defining the exit medium, defaults to Layer.from_material()
+        :type exit_layer: Layer, optional
+        :param num_periods: Number of repetitions of the unit cell, defaults to 1
+        :type num_periods: int, optional
+        :param copy_layers: Whether to deepcopy every Layer, defaults to True
+        :type copy_layers: bool, optional
+        :return: Instance of Multilayer
+        :rtype: Multilayer
+        """
+        layers = []
+        if copy_layers:
+            layers.append(copy.deepcopy(incident_layer))
+            for _ in num_periods:
+                for layer in unit_cell:
+                    layers.append(copy.deepcopy(layer))
+            layers.append(copy.deepcopy(exit_layer))
+        else:
+            layers.append(incident_layer)
+            for _ in num_periods:
+                for layer in unit_cell:
+                    layers.append(layer)
+            layers.append(exit_layer)
+        return cls(layers)
