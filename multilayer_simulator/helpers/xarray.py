1cc53f7463235786a003482adcd6032810a06915
Alex Qiu <alex.qiu@bristol.ac.uk>
2023-01-23 17:30:33 +0000

    Add: add_unpolarised_to_xarray_dataset

--- multilayer_simulator/helpers/xarray.py
+++ multilayer_simulator/helpers/xarray.py
@@ -2,6 +2,8 @@ from typing import Hashable, Iterable, Union
 import xarray as xr
 import numpy as np
 
+DatasetKeys = Union[Iterable[Hashable], Hashable]
+
 
 def vector_norm(x, dim, ord=None):
     return xr.apply_ufunc(
@@ -22,7 +24,12 @@ def add_vector_norms_to_xarray_dataset(dataset: xr.Dataset, dim):
         dataset[f"|{variable}|^2"] = vector_norm(dataset[variable], dim)
 
 
-def add_absorption_to_xarray_dataset(dataset: xr.Dataset, reflectance_key: Union[Iterable, Hashable], transmittance_key: Union[Iterable, Hashable], absorptance_key: Union[Iterable, Hashable]):
+def add_absorption_to_xarray_dataset(
+    dataset: xr.Dataset,
+    reflectance_key: DatasetKeys,
+    transmittance_key: DatasetKeys,
+    absorptance_key: DatasetKeys,
+) -> xr.Dataset:
     """
     Modify a dataset by calculating the absorptance from the DataArrays indicated by the reflectance and transmittance keys, and
     adding it to the dataset under the absorptance key. Returns the modified dataset as a copy.
@@ -31,18 +38,55 @@ def add_absorption_to_xarray_dataset(dataset: xr.Dataset, reflectance_key: Union
     :param dataset: An xarray dataset
     :type dataset: xr.Dataset
     :param reflectance_key: Key or sequence of keys of reflectance variable.
-    :type reflectance_key: Union[Iterable, Hashable]
+    :type reflectance_key: DatasetKeys
     :param transmittance_key: Key or sequence of keys of transmittance variable.
-    :type transmittance_key: Union[Iterable, Hashable]
+    :type transmittance_key: DatasetKeys
     :param absorptance_key: Key or sequence of keys of absorptance variable.
-    :type absorptance_key: Union[Iterable, Hashable]
+    :type absorptance_key: DatasetKeys
     """
     r_keys = np.atleast_1d(reflectance_key)
     t_keys = np.atleast_1d(transmittance_key)
     a_keys = np.atleast_1d(absorptance_key)
-    
-    variables = {a: lambda x: 1 - x[r] - x[t] for r, t, a in zip(r_keys, t_keys, a_keys)}
-    
+
+    variables = {
+        a: lambda x: 1 - x[r] - x[t] for r, t, a in zip(r_keys, t_keys, a_keys)
+    }
+
     modified = dataset.assign(variables=variables)
-    
+
+    return modified
+
+
+def add_unpolarised_to_xarray_dataset(
+    dataset: xr.Dataset,
+    s_key: DatasetKeys,
+    p_key: DatasetKeys,
+    unpolarised_key: DatasetKeys,
+) -> xr.Dataset:
+    """
+    Modify a dataset by calculating the unpolarised variable from the DataArrays indicated by the s- and p-polarisation keys,
+    and adding it to the dataset under the unpolarised key. Return the modified dataset as a copy.
+    TODO: Raise warning if either the reflectance or transmittance keys are missing.
+
+    :param dataset: An xarray dataset
+    :type dataset: xr.Dataset
+    :param s_key: Key or sequence of keys of s_polarised variable.
+    :type s_key: DatasetKeys
+    :param p_key: Key or sequence of keys of p_polarised variable.
+    :type p_key: DatasetKeys
+    :param unpolarised_key: Key or sequence of keys of unpolarised variable.
+    :type unpolarised_key: DatasetKeys
+    :return: Modified dataset
+    :rtype: xr.Dataset
+    """
+    s_keys = np.atleast_1d(s_key)
+    p_keys = np.atleast_1d(p_key)
+    u_keys = np.atleast_1d(unpolarised_key)
+
+    variables = {
+        u: lambda x: (x[s] + s[p]) / 2 for s, p, u in zip(s_keys, p_keys, u_keys)
+    }
+
+    modified = dataset.assign(variables=variables)
+
     return modified
